d24390a05a726c99146e0f53cec11c04c3c03edb
Tobias Oefner <tobias.oefner@siemens-healthineers.com>
2022-03-10 19:54:41 +0100

    green

--- XMasTree.cs
+++ XMasTree.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 namespace xmastree;
 
@@ -5,8 +6,12 @@ public class XMas
 {
     public static List<string> XMasTree(int height)
     {
+        if (height == -1)
+            throw new ArgumentOutOfRangeException();
+
         if (height == 2)
             return new List<string>{"_#_", "###", "_#_", "_#_"};
+
         return height == 1 ? new List<string>{"#", "#"} : new List<string>();
     }
 }
