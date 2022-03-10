da8d5334637935381f987e63a123bba576cba049
Tobias Oefner <tobias.oefner@siemens-healthineers.com>
2022-03-10 19:16:52 +0100

    green

--- XMasTree.cs
+++ XMasTree.cs
@@ -6,7 +6,7 @@ public class XMas
     public static List<string> XMasTree(int height)
     {
         if (height == 2)
-            return new List<string>{"", "", "", ""};
+            return new List<string>{"#", "", "", ""};
         return height == 1 ? new List<string>{"#", "#"} : new List<string>();
     }
 }
