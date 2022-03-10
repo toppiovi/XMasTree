99ac5821a9539cc12ecc7d77c3de7f21997b117d
Tobias Oefner <tobias.oefner@siemens-healthineers.com>
2022-03-10 18:49:05 +0100

    init

---
+++ XMasTreeTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace xmastree;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
