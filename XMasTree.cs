57125ba3c2d681d502b590d774052a12759506f3
Tobias Oefner <tobias.oefner@siemens-healthineers.com>
2022-03-10 18:56:18 +0100

    red

---
+++ XMasTree.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace xmastree;
+
+public class XMas 
+{
+    public static List<string> XMasTree(int height)
+    {
+        return new List<string>();
+    }
+}
--- XMasTreeTests.cs
+++ XMasTreeTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 namespace xmastree;
+using static XMas;
 
 public class XMasTreeTests
 {
@@ -8,5 +9,11 @@ public class XMasTreeTests
     public void Height_0_creates_an_empty_list()
     {
         CollectionAssert.AreEqual(new List<string>(), XMasTree(0));
+    }
+
+    [Test]
+    public void Height_1_creates_a_tree_with_a_trunk_of_height_1_and_a_crown_of_height_1()
+    {
+        CollectionAssert.AreEqual(new List<string>{"#", "#"}, XMasTree(1));
     }
 }
